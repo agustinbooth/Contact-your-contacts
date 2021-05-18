9018c3b1648513038d4a7fa7f06d1c4268ca76ec
agustinvd10 <avalenzu@chicagobooth.edu>
2021-05-18 22:40:02 +0000

    model_memberships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_223655) do
+ActiveRecord::Schema.define(version: 2021_05_18_223920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_05_18_223655) do
 
   create_table "groups", force: :cascade do |t|
     t.string "group_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "contact_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
