75bb16e2303ceed5d50db2277d0107981648a3da
agustinvd10 <avalenzu@chicagobooth.edu>
2021-05-19 20:51:04 +0000

    ransack working!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,8 @@ class ContactsController < ApplicationController
 
   # GET /contacts or /contacts.json
   def index
-    @contacts = Contact.all
+  @q = Contact.ransack(params[:q])
+  @contacts = @q.result
   end
 
   # GET /contacts/1 or /contacts/1.json
