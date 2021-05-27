d6c1cd21f5c344e06023bbc333a3040dc771413b
agustinvd10 <avalenzu@chicagobooth.edu>
2021-05-27 23:29:09 +0000

    used annotate

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id                    :bigint           not null, primary key
+#  name                  :string
+#  last_name             :string
+#  phone_number          :string
+#  communication_channel :string
+#  user_id               :integer
+#  email                 :string
+#  organization          :string
+#  facebook              :string
+#  instagram             :string
+#  twitter               :string
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
 require "test_helper"
 
 class ContactTest < ActiveSupport::TestCase
--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: memberships
+#
+#  id         :bigint           not null, primary key
+#  group_id   :integer
+#  contact_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class MembershipTest < ActiveSupport::TestCase
