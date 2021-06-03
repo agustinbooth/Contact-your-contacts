278eb033b224befaeb6f757e4e44d9ffe2fb7791
agustinvd10 <77352071+agustinvd10@users.noreply.github.com>
2021-06-03 15:07:42 -0500

    Ajaxified app (#8)
    * Ajaxified the group index, now you can:
    
    create, edit and delete groups in an ajaxified way
    create and delete membership through ajax
    
    * trying to solve merge problem
    
    * Update app/controllers/memberships_controller.rb
    
    Co-authored-by: Jelani Woods <jelani@firstdraft.com>
    
    * Update app/views/groups/_group_card.html.erb
    
    Co-authored-by: Jelani Woods <jelani@firstdraft.com>
    
    * comments addressed and everything seems to be working in this pull request
    
    Co-authored-by: Jelani Woods <jelani@firstdraft.com>

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id         :bigint           not null, primary key
+#  group_name :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
 class Group < ApplicationRecord
   has_many  :memberships, dependent: :destroy
   has_many :contacts, through: :memberships, source: :contact
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id         :bigint           not null, primary key
+#  group_name :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
 require "test_helper"
 
 class GroupTest < ActiveSupport::TestCase
