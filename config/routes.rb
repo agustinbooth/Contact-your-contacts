3bb6a3ef0b746fe102f6bd736fab0caa63ecf2b2
agustinvd10 <avalenzu@chicagobooth.edu>
2021-06-04 19:32:08 +0000

    comments addressed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :memberships, only: [:new, :create, :destroy]
   devise_for :users
-  resources :groups, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :contacts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :groups
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
